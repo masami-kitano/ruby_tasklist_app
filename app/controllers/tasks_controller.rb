4ba7659568b8b523d047305324057a1c3635e8e1
m.kitano <11base.movie@gmail.com>
2020-01-14 13:02:39 +0000

    modify task_show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,8 +7,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
-    @task = @user.tasks.order(id: :desc).page(params[:page])
+    @tasks = Task.find(params[:id])
   end
   
   def new
